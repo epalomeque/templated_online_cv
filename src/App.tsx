9906ba01dbd54bff3dc7672721f3c9dda7dac531
Emmanuel Palomeque Alcázar <epalomeque@gmail.com>
2025-01-20 01:48:15 -0600

    add simple resume component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-import {default as cvData} from "./assets/cvdata.json";
+import { default as cvData } from "./assets/cvdata.json";
+import SimpleResume from "./components/single-theme/simple_resume.tsx";
 
 function App() {
   const title = import.meta.env.VITE_APP_TITLE;
-  // const dataFile = import.meta.env.VITE_JSON_FILE;
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>{title}</h1>
       <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
         <p>
           JSON File <code>{cvData.personal_info.name} {cvData.personal_info.lastname}</code>
         </p>
@@ -29,6 +16,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <SimpleResume />
     </>
   )
 }
---
+++ src/components/single-theme/simple_resume.tsx
@@ -0,0 +1,3 @@
+export default function SimpleResume () {
+  return <div>testo</div>;
+}
