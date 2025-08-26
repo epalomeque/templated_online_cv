b6ddedd8c00e714356aeabb8d27bd515f8610081
Emmanuel Palomeque Alcázar <epalomeque@gmail.com>
2025-08-26 15:32:24 -0600

    minor adjustments for config files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,6 @@
-/// <reference types="vite/client" />
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
     readonly VITE_JSON_FILE: string
-    // more env variables...
 }
 
 interface ImportMeta {
