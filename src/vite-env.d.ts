0b6373b7be68387220c5f77a632985e0c1c11f9b
Emmanuel Palomeque <epalomeque@gmail.com>
2026-02-09 23:36:36 -0600

    fixed lost vite file

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_TITLE: string
+    readonly VITE_JSON_FILE: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
+
+/// <reference types="vite/client" />
