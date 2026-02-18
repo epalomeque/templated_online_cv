8349d9f4f2a35c98ef91b3a3a7c7e42fc1d9fb78
Emmanuel Palomeque <epalomeque@gmail.com>
2026-02-18 13:24:19 -0600

    change in footer

--- src/features/cv-editor/components/ResumeActionsFooter.tsx
+++ src/features/cv-editor/components/ResumeActionsFooter.tsx
@@ -10,7 +10,7 @@ const ResumeActionsFooter: React.FC<ResumeActionsFooterProps> = () => {
     const app_settings = getAppSettings();
     return (
         <div className="resume-actions-footer-container">
-            <h4 className="resume-footer-url">Github: { app_settings.githubHostedUrl }</h4>
+            <h4 className="resume-footer-url"><a href={ app_settings.githubHostedUrl }>Repositorio en Github</a> | <a href='https://epalomeque.net'>epalomeque.net</a> </h4>
         </div>
     );
 };
