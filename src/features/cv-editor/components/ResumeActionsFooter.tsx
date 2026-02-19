7d76b1b9ce00de738568df28b9ca8a43159e717e
Emmanuel Palomeque <epalomeque@gmail.com>
2026-02-18 18:50:20 -0600

    minor bug fixes and styling

--- src/features/cv-editor/components/ResumeActionsFooter.tsx
+++ src/features/cv-editor/components/ResumeActionsFooter.tsx
@@ -9,8 +9,10 @@ interface ResumeActionsFooterProps {
 const ResumeActionsFooter: React.FC<ResumeActionsFooterProps> = () => {
     const app_settings = getAppSettings();
     return (
-        <div className="resume-actions-footer-container">
-            <h4 className="resume-footer-url"><a href={ app_settings.githubHostedUrl }>Repositorio en Github</a> | <a href='https://epalomeque.net'>epalomeque.net</a> </h4>
+        <div className="resume-actions-scope">
+            <div className="resume-actions-footer-container">
+                <h4 className="resume-footer-url"><a href={ app_settings.githubHostedUrl }>Repositorio en Github</a> | <a href='https://epalomeque.net'>epalomeque.net</a> </h4>
+            </div>
         </div>
     );
 };
